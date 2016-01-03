9772cf0d9cca228610141b64c371ec06c85cb3e2
megamorf <basti.megamorf+github-com@googlemail.com>
2016-01-03 14:37:18 +0100

    added more files and updates

---
+++ Emergency/Mass-Unblock-Users.ps1
@@ -0,0 +1,4 @@
+﻿## Find every account locked out in the domain that was locked out today
+$users = Search-ADAccount -LockedOut -UsersOnly | Where-Object { [datetime]::FromFileTime((get-aduser $_ -Properties lockouttime).lockouttime).Date -eq (Get-Date).Date }
+## Write the username to the console while unlocking each account in case you want to see your results
+$users | ForEach-Object { $_.samAccountName; Unlock-AdAccount $_.samAccountName }
---
+++ Regex/CaptureGroups.ps1
@@ -0,0 +1,22 @@
+﻿#To fill an array $words with just the matches, use the command
+$string1 = "woo hoo, let's go to the zoo."
+$words1 = ([regex]::matches($string1, "\woo") | %{$_.value})
+$words1
+
+$string2 = "Cookbook"
+$words2 = ([regex]::matches($string2, "\woo\w") | %{$_.value})
+$words2
+
+# For example, the code will match “Cook” and “book“, whereas
+# without the IgnoreCase option only “book” would match.
+[regex]::matches($string2, "[a-z]ook", "IgnoreCase")
+
+
+# Notice the single quotes around the replacement pattern.
+# This is to keep PowerShell from interpreting “$1” before
+# passing passing it to the Regex class. We could use double
+# quotes if we also put back ticks in front of the dollar
+# signs to escape them.
+$string3 = “<i>big</i>”
+$words3 = [regex]::Replace($string3 , "<i>(\w+)</i>", '$1 $1')
+$words3
