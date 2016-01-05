a132e74ee828f886b8a573d5fc36ba00ab78ae82
Administrator <xnem@outlook.com>
2016-01-05 07:26:17 +0100

    added user mgmt functions from @adbertram

---
+++ Pluralsight - Building Tools with PowerShell/Script.ps1
@@ -0,0 +1,42 @@
+﻿## Example Active Directory Account Automator #1: A Script ##
+
+$Employees = Import-Csv -Path C:\Users.csv ## BAD: File path in the script itself
+foreach ($Employee in $Employees)
+{
+	#region Create the AD user account
+    ## BAD: What if the desired username is taken? No validation here to check and
+	## try to use another scheme
+	$Username = "$($Employee.FirstName.SubString(0, 1))$($Employee.LastName)"
+	$NewUserParams = @{
+		'Title' = $Employee.Title
+        'UserPrincipalName' = $Username
+		'Name' = $Username
+		'GivenName' = $Employee.FirstName
+		'Surname' = $Employee.LastName
+		'SamAccountName' = $Username
+        'Department' = $Employee.Department
+		## Password in clear text (not solving this in this webinar) and it's in the
+		## script. BAD: This should be a parameter.
+		'AccountPassword' = (ConvertTo-SecureString 'p@$$w0rd12' -AsPlainText -Force)
+		'Enabled' = $true
+		'Initials' = $Employee.MiddleInitial
+		'ChangePasswordAtLogon' = $true
+	}
+	if ($Employee.UserOU)
+	{
+		$NewUserParams.Path = "$($Employee.UserOU),DC=lab,DC=local" ## What if we need another domain?
+	}
+	
+	New-AdUser @NewUserParams
+	#endregion
+	
+	#region Add the user to the standard group
+	## BAD: Group name in the script.
+	Add-ADGroupMember -Identity 'Gigantic Corporation Inter-Intra Synergy Group' -Members $Username
+	#endregion
+	
+	#region Create the employee's AD computer account
+	## BAD: Static OU path in the script
+	New-ADComputer -Name $Employee.Computername -Path 'OU=Corporate Computers,DC=lab,DC=local'
+	#endregion
+}
---
+++ Pluralsight - Building Tools with PowerShell/Tool.ps1
@@ -0,0 +1,69 @@
+﻿## Example Active Directory Account Automator #1: A Tool ##
+
+[CmdletBinding()] ## We're talkin' advanced here
+param (
+	[Parameter(Mandatory)] ## Parameters are a great way to prevent from modifying the script
+	[ValidateNotNullOrEmpty()]
+	[ValidateScript({ Test-Path -Path $_ -PathType Leaf })] ## Validation
+	[string]$EmployeesCsv,
+
+	[Parameter()]
+	[ValidateNotNullOrEmpty()]
+	[ValidateScript({ Test-Path -Path $_ -PathType Leaf })] ## Validation
+	[string]$FunctionsScript = "C:\Dropbox\Powershell\GitRepos\Session-Content\Webinars\Pluralsight - Building Tools with PowerShell\AdAccountManagementAutomator.ps1"
+)
+
+try
+{
+	## Take out code that is reused over and over into functions and preferably get them out
+	## out of your main script into a script with functions (or a module)
+	. $FunctionsScript
+	
+	$Employees = Import-Csv -Path $EmployeesCsv
+	if ($Employees) ## Checking to see if there was actually any employees in the CSV
+	{
+		foreach ($Employee in $Employees)
+		{
+			try ## A try/catch blog to catch errors
+			{
+				#region Create AD user account and add to standard group
+                ## Far fewer parameters
+				$NewUserParams = @{
+					'FirstName' = $Employee.FirstName
+					'MiddleInitial' = $Employee.MiddleInitial
+					'LastName' = $Employee.LastName
+					'Title' = $Employee.Title
+                    'Department' = $Employee.Department
+                    'Verbose' = $VerbosePreference
+				}
+				if ($Employee.UserOU)
+				{
+					$NewUserParams.Location = $Employee.UserOU
+				}
+
+				New-EmployeeOnboardUser @NewUserParams 
+				#endregion
+				
+                $newComputersParams = @{
+                    'ComputerName' = $Employee.Computername
+                    'Verbose' = $VerbosePreference
+                }
+				#region Create the employee's AD computer account
+				New-EmployeeOnboardComputer @newComputersParams
+				#endregion
+			}
+			catch
+			{
+				Write-Error $_.Exception.Message
+			}
+		}
+	}
+	else
+	{
+		Write-Warning "No records found in the file [$($EmployeesCsv)]"	
+	}
+}
+catch
+{
+	Write-Error $_.Exception.Message ## Error handling
+}
